d9406b6fc79348b76d2a3879663caf83c2ff6109
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-28 12:52:41 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,16 @@
-// Code SimpleComponent Here
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+  state = {
+    mood: 'happy',
+  };
+
+  handleClick = () =>
+    this.state.mood === 'happy' ? this.setState({ mood: 'sad' }) : this.setState({ mood: 'happy' });
+  toggleSize = () =>
+    this.state.size === 20 ? this.setState({ size: 50 }) : this.setState({ size: 20 });
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
