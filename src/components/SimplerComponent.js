b354bfc70b4ac468128480de96d16c740e97fa3e
Trevahr Robertson <Trevahr@gmail.com>
2019-08-28 21:22:26 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,19 @@
 // Code SimplerComponent Here
+
+import React from "react";
+
+// handleClick = () =>{
+//     console.log(event)
+//   }
+
+ const SimplerComponent = (props) => {
+    
+    return (
+    <div onClick={(event) => props.handleClick(event)}>
+        I am just happy
+    </div>
+    )
+}
+
+
+export default SimplerComponent
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import SimpleComponent from "./components/SimpleComponent";
+import SimplerComponent from "./components/SimplerComponent";
+
+
+handleClick = (event) => {
+  console.log(event)
+}
+
 
 ReactDOM.render(
+
+
+
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(event) => this.handleClick(event)} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
