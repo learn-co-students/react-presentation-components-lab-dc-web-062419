0d6601444654627ad172da79c1ef48d6fc44dc5a
github username <github email address>
2019-09-03 18:00:04 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,15 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = (props) => {
+
+    // const aFunction = () => {
+    //     console.log("I'm a fucking Unicorn")        
+    // }
+
+    return (
+         <div onClick={props.handleClick}>I am just happy</div>
+    )
+}
+
+export default SimplerComponent
